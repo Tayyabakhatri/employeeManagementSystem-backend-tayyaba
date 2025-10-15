8abde2c06d40debf07cfda1bd25b380b658f3e7f
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-10-15 21:22:35 +0500

    modified

--- app.mjs
+++ app.mjs
@@ -6,7 +6,11 @@ import 'dotenv/config';
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 
 
 // apis
